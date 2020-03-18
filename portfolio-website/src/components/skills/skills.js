5688395a3afc284fa499bd29eaaa8d0baccf43be
AaronBowles <Bowles@Aarons-MacBook-Pro.local>
2020-03-18 18:29:02 -0400

    began styling and creating skills component

--- portfolio-website/src/components/skills/skills.js
+++ portfolio-website/src/components/skills/skills.js
@@ -7,7 +7,28 @@ class Skills extends Component {
         return (
           <div className="skillsContainer">
 
-            <h1> skills</h1>
+            <h1 className="skillsHeader"> Skills </h1>
+
+            <div className="skills">
+                <h3>HTML</h3>
+                <h3>CSS</h3>
+                <h3>Git</h3>
+            </div>
+            <div className="javaScriptSkills">
+              <h3>JavaScript</h3>
+              <h3>React</h3>
+              <h3>Node</h3>
+            </div>
+            <div className="pythonSkills">
+                <h3>Python</h3>
+                <h3>Django</h3>
+            </div>
+            <div className="databaseSkills">
+                <h3>SQL</h3>
+                <h3>Postgres</h3>
+                <h3>MongoDB</h3>
+                <h3>Oracle</h3>
+            </div>
           </div>
         )
     }
