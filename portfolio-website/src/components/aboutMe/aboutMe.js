570a9eccf22cf6c9d9f977ce285c199439131b09
AaronBowles <Bowles@aarons-mbp.myfiosgateway.com>
2020-10-10 16:08:09 -0400

    adjusted CSS for intro and added additional info to about me component

--- portfolio-website/src/components/aboutMe/aboutMe.js
+++ portfolio-website/src/components/aboutMe/aboutMe.js
@@ -10,8 +10,8 @@ class AboutMe extends Component {
             <h1 className="aboutTitle"> About Me </h1>
             <div className="aboutImg"></div>
             <div className="aboutInfo">
-                <p>My name is Aaron Bowles, I am a full-stack developer originally from Fairfax, Virginia. My intense passion for technology, science, progress and the environment have shaped my world view and motivated me to become a software developer. The endless possibilities of software developing have opened my eyes to an abundance of opportunities doing what I love. Helping others while growing as an individual is a rewarding and fulfilling pursuit that continues each and everyday.  </p>
-                <p>This will be additional info </p>
+                <p className="aboutPara">My name is Aaron Bowles, I am a full-stack developer originally from Fairfax, Virginia. My intense passion for technology, science, progress and the environment have shaped my world view and motivated me to become a software developer. The endless possibilities of software developing have opened my eyes to an abundance of opportunities doing what I love. Helping others while growing as an individual is a rewarding and fulfilling pursuit that continues each and everyday.  </p>
+                
             </div>
           </div>
         )
--- portfolio-website/src/components/intro/intro.js
+++ portfolio-website/src/components/intro/intro.js
@@ -6,10 +6,12 @@ class Intro extends Component {
     render() {
         return (
           <div className='introContainer'>
+            <div className='greetingContainer'>
 
             <h1 className='greeting'> Greetings! </h1>
             <h3 className='introduction'>Hope you enjoy this background picture I took!</h3>
 
+            </div>
           </div>
         )
     }
