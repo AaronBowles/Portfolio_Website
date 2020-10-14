ad78b6daba3f0299d32441ac1f428807786a9fd2
AaronBowles <Bowles@aarons-mbp.myfiosgateway.com>
2020-10-13 21:01:02 -0400

    adjusted css for projects

--- portfolio-website/src/components/projects/projects.js
+++ portfolio-website/src/components/projects/projects.js
@@ -5,24 +5,27 @@ class Projects extends Component {
 
     render() {
         return (
-          <div className='projectsContainer'>
+          <div className='projects-container'>
             
             <h1> projects</h1>
+          <div className='projects-flexbox'>
 
-            <div className='carousel-container'>
               <div className='carousel-prev'>
                 <button>prev</button>
               </div>
+
+              <div className='carousel-container'>
         
+              <div className="carousel-projects">
+              <img src={require("../../images/Project1Screenshot.png")} alt="Project1" className="project-images"/>
+              </div>
 
-            <div className="carousel-projects">
-            <img src={require("../../images/Project1Screenshot.png")} alt="Project1" className="project-images"/>
+              
             </div>
-
               <div className='carousel-next'>
                 <button>next</button>
               </div>
-            </div>
+          </div>
             
           </div>
         )
